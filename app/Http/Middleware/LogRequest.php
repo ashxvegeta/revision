1d2ae9b1ab5d7c430419f8015e6cc29c8d8f0c33
ashxvegeta <ashutoshroy453@gmail.com>
2026-06-21 12:03:37 +0530

    DAY 1 (Create a simple middleware)

---
+++ app/Http/Middleware/LogRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogRequest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (app()->environment('local')) {
+        \Log::info('Request Recived'.$request->url());
+        }
+        return $next($request);
+    }
+}
