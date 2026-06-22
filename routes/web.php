c1505f6face1afbf2820e5b0dcbb77f7aa2da411
ashxvegeta <ashutoshroy453@gmail.com>
2026-06-22 22:47:12 +0530

    DAY 5 FACADES

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,12 @@ Route::get('/', function () {
 // same route bhi use hoga
 Route::get('/pay', [PaymentController::class, 'charge'])
     ->middleware(\App\Http\Middleware\LogRequest::class);
+
+
+Route::get('/facade-test',function(){
+       $user  = Auth::user();
+       \Log::info('Auth Facade test',['user'=>Auth::user()]);
+      \Log::info('App environment', ['env' => App::environment()]);
+       \Log::info('Request methods',['methods'=>Request::method()]);
+       return response()->json(['message'=>'Facades kaam kiya']);
+});
